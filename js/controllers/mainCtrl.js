43d36250a2a1ecb269204b1623a0731a8ff4f82d
devinz1 <devinzohner@gmail.com>
2014-11-15 13:30:10 -0700

    trying to figure out percentage save in progress bar via firebase

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -6,33 +6,19 @@ angular.module('ui.bootstrap.demo').controller('ProgressDemoCtrl', function ($sc
 
   $scope.raised = $scope.progressResult.raised;
   $scope.goal = $scope.progressResult.goal;
+  $scope.percentage = $scope.progressResult.percentage;
   $scope.getPercentage = function(){
-    return (($scope.raised / $scope.goal) * 100);
+   // return (($scope.raised / $scope.goal) * 100);
+   $scope.percentage = $scope.raised / $scope.goal * 100;
+
   }
+
+  var saveMoney = sync.$add();
+  $scope.progressResult = saveMoney;
 // this is past code I tried out, not sure if I want to delete yet just in case I go back to it.
-  $scope.max = 200;
+ // $scope.max = 200;
 
-  $scope.random = function() {
-    var value = Math.floor((Math.random() * 100) + 1);
-    var type;
-
-    if (value < 25) {
-      type = 'success';
-    } else if (value < 50) {
-      type = 'info';
-    } else if (value < 75) {
-      type = 'warning';
-    } else {
-      type = 'danger';
-    }
-
-    
-
-    $scope.dynamic = value;
-    $scope.type = type;
-  };
-  $scope.random();
-
+ 
 
 
 //sync.$update("First Aid Kits",{goal:1000, raised:640,});
